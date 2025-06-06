ef8e64574e957d0647dfd0f54944768f51e281fe
LadyCodeX <graceyambo23@gmail.com>
2025-06-07 00:03:06 +0100

    ajout des éléments

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 
-from .route import bulali
+from .view import bulali
 
 data_etudiants = SQLAlchemy(bulali)
 class etudiants(data_etudiants.Model):
 	id= data_etudiants.Column(data_etudiants.Integer, primary_key=True)
-	description = data_etudiants.Colum(data_etudiants.String(200), nullable=False)
-	
+	nom = data_etudiants.Colum(data_etudiants.String(100), nullable=False)
+	postnom = data_etudiants.Colum(data_etudiants.String(100), nullable=False)
+	prenom = data_etudiants.Colum(data_etudiants.String(100), nullable=False)
+	photos = data_etudiants.Colum(data_etudiants.String(200), nullable=False)
+	encodage= data_etudiants.Colum(data_etudiants.String(300), nullable=False)
