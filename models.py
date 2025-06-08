8180db30a6628995bdab0a145ef2bc796392acc6
LadyCodeX <graceyambo23@gmail.com>
2025-06-08 18:53:26 +0100

    connexion à ma base mysql

--- models.py
+++ models.py
@@ -1,30 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
 import logging as lg
 from .view import bulali
+import os
+
+
+bulali.config['SQLALCHEMY_DATABASE_URI']= 'mysql+mysqlconnector://root:Gra26ce10@localhost/data_etudiants'
+bulali.config['SQLAlCHEMY_TRACK_MODIFICATIONS']= False
 
 data_etudiants = SQLAlchemy(bulali)
+ 
+dossier_visages= "static/images_bulali"
+os.makedirs(dossier_visages, exist_ok=True)
 
-class etudiants(data_etudiants.Model):
+class Etudiants(data_etudiants.Model):
+	__tablename__='etudiants'
+
 	id= data_etudiants.Column(data_etudiants.Integer, primary_key=True)
 	nom = data_etudiants.Colum(data_etudiants.String(100), nullable=False)
 	postnom = data_etudiants.Colum(data_etudiants.String(100), nullable=False)
 	prenom = data_etudiants.Colum(data_etudiants.String(100), nullable=False)
+	matricule = data_etudiants.Column(data_etudiants.String(50), unique= True, nullable=False)
 	photos = data_etudiants.Colum(data_etudiants.String(200), unique=True, nullable=False)
 	encodages= data_etudiants.Colum(data_etudiants.String(300), unique=True, nullable=False)
 
-	def _init_(self, nom, postnom, prenom, photos, encodages):
-		self.nom = nom
-		self.postnom = postnom
-		self.prenom = prenom
-		self.photos = photos
-		self.encodages = encodages
 		
-data_etudiants.create_all()
 
-def init_db():
-	data_etudiants.drop_all()
-	data_etudiants.create_all()
-	data_etudiants.session.add(etudiants("c'est partaa", 1))
-	data_etudiants.session.add(etudiants("moi toi toi moi", 0))
-	data_etudiants.session.commit()
-	lg.warning('Database initialisé')
+
+
+
